f4661c32a4c24f67e011ce581d94b8d9d323fb34
Rahul110493 <38890875+Rahul110493@users.noreply.github.com>
2021-06-22 14:35:47 +0530

    Create main.tf

---
+++ tf-scripts/main.tf
@@ -0,0 +1,45 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google-beta"
+      version = "3.70.0"
+    }
+  }
+}
+provider "google" {
+  project = "${var.project_id}"
+  region  = "${var.region}"
+}
+
+resource "google_cloud_run_service" "default" {
+  name     = "cloudrun-opt-out"
+  location = "${var.region}"
+
+  template {
+    spec {
+      containers {
+        image = "${var.docker_image}"
+        ports {
+          container_port = 80
+         }        
+      }
+    }
+  }
+}
+
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location    = google_cloud_run_service.default.location
+  project     = google_cloud_run_service.default.project
+  service     = google_cloud_run_service.default.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
+}

a5185d6123300f2a185de612811fbf76988980e5
Rahul110493 <38890875+Rahul110493@users.noreply.github.com>
2021-09-27 13:11:31 +0530

    Update main.tf

--- tf-scripts/main.tf
+++ tf-scripts/main.tf
@@ -48,43 +48,6 @@ module "gcp-cloudrun" {
   
 }
 
-module "gcp-gke" {
-    count = var.vendor == "gcp" && var.vendor_service == "gke" ? 1 : 0
-    source = "./tf-scripts/gcp/service/gke"
-
-    project_id = var.project_id
-    region = var.region
-    zone = var.zone
-    cls_name = var.cls_name
-    cls_location_id = var.cls_location_id
-    remove_default_node_pool = var.remove_default_node_pool
-    initial_node_count = var.initial_node_count
-    container_node_pool_name = var.container_node_pool_name
-    container_node_pool_name_location_id = var.container_node_pool_name_location_id
-    node_count = var.node_count
-    preemptible = var.preemptible
-    machine_type = var.machine_type
-    
-  
-}
-
-  module "gcp-cloudfunctions" {
-  count = var.vendor == "gcp" && var.vendor_service == "cloudfunctions" ? 1 : 0
-  source = "./tf-scripts/gcp/service/cloudfunctions"
-        
-  function_name         = var.function_name    
-  description           = var.description != "" ? var.description : "${var.name} HTTP Cloud Function"
-  available_memory_mb   = var.available_memory_mb
-  bucket_name           = var.bucket_name
-  timeout               = var.timeout
-  entry_point           = var.entry_point
-  runtime               = var.runtime
-  environment_variables = var.environment_variables
-  service_account_email = var.service_account_email
-  vpc_connector         = var.vpc_connector
-  max_instances         = var.max_instances
-}
-    
 module "alicloud-ack" {
     count = var.vendor == "alicloud" && var.vendor_service == "ack" ? 1 : 0
     source = "./tf-scripts/alicloud/service/ack"

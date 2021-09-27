a1d7d4630e82b345af63d08865370618fbf7e9f3
Rahul110493 <38890875+Rahul110493@users.noreply.github.com>
2021-09-27 12:06:33 +0530

    Create variables.tf

---
+++ tf-scripts/tf-scripts/gcp/service/service-account/variables.tf
@@ -0,0 +1,13 @@
+variable "service_account_id" {
+  description = "The service_account_id of an Google Project"
+  type = string
+  default = ""
+}
+
+variable "service_account_name" {
+  description = "The service_account_name of an Google Project"
+  type = string
+  default = ""
+}
+
+variable "sa_permission_list" {}

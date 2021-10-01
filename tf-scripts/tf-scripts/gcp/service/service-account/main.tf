3cc1ddad77c6ef0ae05fd91aa154812a71cf0df3
Rahul110493 <38890875+Rahul110493@users.noreply.github.com>
2021-10-01 16:12:39 +0530

    Update main.tf

--- tf-scripts/tf-scripts/gcp/service/service-account/main.tf
+++ tf-scripts/tf-scripts/gcp/service/service-account/main.tf
@@ -2,9 +2,3 @@ resource "google_service_account" "service_account" {
   account_id   = var.service_account_id
   display_name = var.service_account_name
 }
-
-resource "google_project_iam_member" "service_account_permissions" {
-  for_each =  toset(var.sa_permission_list)
-  role     =  each.value
-  member   = "serviceAccount:${google_service_account.service_account.email}"
-}

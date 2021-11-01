5c4e0a2c6bc26c27c09df4853fc0275fe8e71a6c
Rahul110493 <38890875+Rahul110493@users.noreply.github.com>
2021-11-01 17:47:06 +0530

    Update main.tf

--- tf-scripts/tf-scripts/gcp/service/api-resources/main.tf
+++ tf-scripts/tf-scripts/gcp/service/api-resources/main.tf
@@ -1,4 +1,5 @@
 resource "google_project_service" "project-apis" {
+  project = "nodejsapp-324007"
   for_each = toset(var.gcp_service_list)
   service = each.value
   disable_dependent_services = true
